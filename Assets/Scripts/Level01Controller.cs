185cbb5b0215a4614e53fbbf91c86b0ce4dfdcac
Daniel <dancoches@gmail.com>
2020-09-27 21:30:04 -0500

    Full game loop

---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level01Controller : MonoBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitLevel();
+        }
+    }
+
+    public void ExitLevel()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
