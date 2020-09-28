b12e97a687e0469d76420441ff86932f3ad2861b
Daniel <dancoches@gmail.com>
2020-09-27 23:33:03 -0500

    Score Implemented

--- Assets/Scripts/Level01Controller.cs
+++ Assets/Scripts/Level01Controller.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Level01Controller : MonoBehaviour
 {
-    // Update is called once per frame
+    [SerializeField] Text _currentScoreTextView;
+
+    int _currentScore;
+
     private void Update()
     {
+        //INCREASE SCORE//
+        //TODO replace with real implementation; this is for test purposes
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+        //EXIT LEVEL//
+        //TODO bring up navigation pop-up menu
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             ExitLevel();
@@ -18,4 +30,13 @@ public class Level01Controller : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
+
+    public void IncreaseScore(int scoreIncrease)
+    {
+        //increase score
+        _currentScore += scoreIncrease;
+        //update score display so new score is shown
+        _currentScoreTextView.text = "Score: " + _currentScore.ToString();
+    }
+
 }
